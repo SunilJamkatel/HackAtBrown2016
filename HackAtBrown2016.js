5bbfeb7edefdde74f66b573bc81edd5f3000474e
Emma Herold <emma@Emmas-MacBook-Pro-3.local>
2016-02-07 10:44:52 -0500

    everythang

--- HackAtBrown2016.js
+++ HackAtBrown2016.js
@@ -65,9 +65,16 @@ Requests.attachSchema(requestSchema);
 
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
+  smoothScroll.init({
+      selector: '[data-scroll]', // Selector for links (must be a valid CSS selector)
+      selectorHeader: '[data-scroll-header]', // Selector for fixed headers (must be a valid CSS selector)
+      speed: 500, // Integer. How fast to complete the scroll in milliseconds
+      easing: 'easeInOutCubic', // Easing pattern to use
+      updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+      offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+      callback: function ( toggle, anchor ) {} // Function to run after scrolling);
+  });
+  console.log("smooth scroll initialized");
 
   // ~*~*~*~*~ HELPER FUNCTIONS ~*~*~*~*~*~
   Template.home.helpers({
@@ -165,6 +172,7 @@ if (Meteor.isClient) {
         Requests.insert(myRequest);
     }
   });
+
 }
 
 
